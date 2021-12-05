e209775fdd8bc8f149c91ab1e0b885b198471a1a
hdd <hdd1073394498@gmail.com>
2021-12-05 16:04:59 +0800

    revise vis

--- labelme/cli/json_to_dataset.py
+++ labelme/cli/json_to_dataset.py
@@ -1,81 +1,91 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#!H:\Anaconda3\envs\new_labelme\python.exe
 import argparse
-import base64
 import json
 import os
 import os.path as osp
-
-import imgviz
+import base64
+import warnings
+ 
 import PIL.Image
-
-from labelme.logger import logger
+import yaml
+ 
 from labelme import utils
-
-
+ 
+import cv2
+import numpy as np
+from skimage import img_as_ubyte
+ 
+# from sys import argv
+ 
 def main():
-    logger.warning(
-        "This script is aimed to demonstrate how to convert the "
-        "JSON file to a single image dataset."
-    )
-    logger.warning(
-        "It won't handle multiple JSON files to generate a "
-        "real-use dataset."
-    )
-
+    warnings.warn("This script is aimed to demonstrate how to convert the\n"
+                  "JSON file to a single image dataset, and not to handle\n"
+                  "multiple JSON files to generate a real-use dataset.")
+ 
     parser = argparse.ArgumentParser()
-    parser.add_argument("json_file")
-    parser.add_argument("-o", "--out", default=None)
+    parser.add_argument('json_file')
+    parser.add_argument('-o', '--out', default=None)
     args = parser.parse_args()
-
+ 
     json_file = args.json_file
-
-    if args.out is None:
-        out_dir = osp.basename(json_file).replace(".", "_")
-        out_dir = osp.join(osp.dirname(json_file), out_dir)
-    else:
-        out_dir = args.out
-    if not osp.exists(out_dir):
-        os.mkdir(out_dir)
-
-    data = json.load(open(json_file))
-    imageData = data.get("imageData")
-
-    if not imageData:
-        imagePath = os.path.join(os.path.dirname(json_file), data["imagePath"])
-        with open(imagePath, "rb") as f:
-            imageData = f.read()
-            imageData = base64.b64encode(imageData).decode("utf-8")
-    img = utils.img_b64_to_arr(imageData)
-
-    label_name_to_value = {"_background_": 0}
-    for shape in sorted(data["shapes"], key=lambda x: x["label"]):
-        label_name = shape["label"]
-        if label_name in label_name_to_value:
-            label_value = label_name_to_value[label_name]
-        else:
-            label_value = len(label_name_to_value)
-            label_name_to_value[label_name] = label_value
-    lbl, _ = utils.shapes_to_label(
-        img.shape, data["shapes"], label_name_to_value
-    )
-
-    label_names = [None] * (max(label_name_to_value.values()) + 1)
-    for name, value in label_name_to_value.items():
-        label_names[value] = name
-
-    lbl_viz = imgviz.label2rgb(
-        lbl, imgviz.asgray(img), label_names=label_names, loc="rb"
-    )
-
-    PIL.Image.fromarray(img).save(osp.join(out_dir, "img.png"))
-    utils.lblsave(osp.join(out_dir, "label.png"), lbl)
-    PIL.Image.fromarray(lbl_viz).save(osp.join(out_dir, "label_viz.png"))
-
-    with open(osp.join(out_dir, "label_names.txt"), "w") as f:
-        for lbl_name in label_names:
-            f.write(lbl_name + "\n")
-
-    logger.info("Saved to: {}".format(out_dir))
-
-
-if __name__ == "__main__":
+ 
+    #freedom
+    list_path = os.listdir(json_file)
+    print('freedom =', json_file)
+    for i in range(0,len(list_path)):
+        path = os.path.join(json_file,list_path[i])
+        if os.path.isfile(path):
+ 
+            data = json.load(open(path))
+            img = utils.img_b64_to_arr(data['imageData'])
+            lbl, lbl_names = utils.labelme_shapes_to_label(img.shape, data['shapes'])
+ 
+            captions = ['%d: %s' % (l, name) for l, name in enumerate(lbl_names)]
+ 
+            lbl_viz = utils.draw_label(lbl, img, captions)
+            out_dir = osp.basename(path).replace('.', '_')
+            save_file_name = out_dir
+            out_dir = osp.join(osp.dirname(path), out_dir)
+ 
+            if not osp.exists(json_file + '\\' + 'labelme_json'):
+                os.mkdir(json_file + '\\' + 'labelme_json')
+            labelme_json = json_file + '\\' + 'labelme_json'
+ 
+            out_dir1 = labelme_json + '\\' + save_file_name
+            if not osp.exists(out_dir1):
+                os.mkdir(out_dir1)
+ 
+            PIL.Image.fromarray(img).save(out_dir1+'\\'+save_file_name+'_img.png')
+            PIL.Image.fromarray(lbl).save(out_dir1+'\\'+save_file_name+'_label.png')
+                
+            PIL.Image.fromarray(lbl_viz).save(out_dir1+'\\'+save_file_name+
+            '_label_viz.png')
+ 
+            if not osp.exists(json_file + '\\' + 'mask_png'):
+                os.mkdir(json_file + '\\' + 'mask_png')
+            mask_save2png_path = json_file + '\\' + 'mask_png'
+            ################################
+            #mask_pic = cv2.imread(out_dir1+'\\'+save_file_name+'_label.png',)
+            #print('pic1_deep:',mask_pic.dtype)
+ 
+            mask_dst = img_as_ubyte(lbl)  #mask_pic
+            print('pic2_deep:',mask_dst.dtype)
+            cv2.imwrite(mask_save2png_path+'\\'+save_file_name+'_label.png',mask_dst)
+            ##################################
+ 
+            with open(osp.join(out_dir1, 'label_names.txt'), 'w') as f:
+                for lbl_name in lbl_names:
+                    f.write(lbl_name + '\n')
+ 
+            warnings.warn('info.yaml is being replaced by label_names.txt')
+            info = dict(label_names=lbl_names)
+            with open(osp.join(out_dir1, 'info.yaml'), 'w') as f:
+                yaml.safe_dump(info, f, default_flow_style=False)
+ 
+            print('Saved to: %s' % out_dir1)
+ 
+if __name__ == '__main__':
+    #base64path = argv[1]
     main()
