abab900a1b31f2c733f338aa292eec61664fb31b
hdd <hdd1073394498@gmail.com>
2021-12-08 21:40:15 +0800

    revise path

--- sbData/json/labelme2coco.py
+++ sbData/json/labelme2coco.py
@@ -2,10 +2,10 @@
 import labelme2coco
 
 # set directory that contains labelme annotations and image files
-labelme_folder = "E:/paper_exm/labelme/sbData/json/object1"
+labelme_folder = "E:/paper_exm/subwayData/abnormalJson"
 
 # set path for coco json to be saved
-save_json_path = "E:/paper_exm/labelme/sbData/json/coco/coco.json"
+save_json_path = "E:/paper_exm/subwayData/abCocoJson/coco.json"
 
 # convert labelme annotations to coco
 labelme2coco.convert(labelme_folder, save_json_path)
